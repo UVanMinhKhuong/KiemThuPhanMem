4f0862dff74bcf0e8a7649e913ce30e8713dbcb8
nqhQuocHung <hungmt2426@gmail.com>
2023-10-23 13:32:58 +0700

    80 chinh sua file KTPM.cpp

--- KTPM.cpp
+++ KTPM.cpp
@@ -3,7 +3,15 @@
 int main() {
 	std::cout << "Hello World!";
 	for (int i = 0; i < 10; i++) {
-		cout << "Nguyen Quoc Hung\n";
+		{
+			cout << "Nguyen Quoc Hung\n";
+			if (i % 2 == 0)
+				cout << "chan\n";
+			else
+			{
+				cout << "le\n";
+			}
+
 	}
 	return 0;
 }
